00a65d07e30387bc07eabd10f63ba6fed255ff4e
omidiora <omidioraemmanuel@gmail.com>
2021-10-01 18:48:05 +0100

    fix the bug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 @extends('shopify-app::layouts.default')
 @section('content')
 <!-- You are: (shop domain name) -->
+<p>You are: {{ Auth::user()->name }}</p>
 @endsection
 @section('scripts')
 @parent
